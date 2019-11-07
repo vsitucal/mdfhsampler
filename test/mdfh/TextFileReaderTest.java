7ac400786407129be05b0a3e2aac6f38751a6a68
88119g <88119g@cxgtpdevlpt13.chi-x.com>
2019-11-07 11:13:00 +0800

    -TextFileReader unit test v2

--- test/mdfh/TextFileReaderTest.java
+++ test/mdfh/TextFileReaderTest.java
@@ -18,24 +18,24 @@ import static org.junit.Assert.*;
  */
 public class TextFileReaderTest {
     
-    public TextFileReaderTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+//    public TextFileReaderTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
 
     /**
      * Test of readFileData method, of class TextFileReader.
@@ -44,7 +44,7 @@ public class TextFileReaderTest {
     public void testReadFileData() {
         System.out.println("readFileData");
         TextFileReader instance = new TextFileReader("sample.txt");
-        String expResult = "hello";
+        String expResult = "00000000,A,123456789,B,002000,   IBM,0002000000,Y\r\n00000000,A,123456790,S,006000,   IBM,0002010000,Y";
         String result = instance.readFileData();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
